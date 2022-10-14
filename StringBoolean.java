42a327afa455a6199710d674b44887c9090bff42
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2022-10-14 12:43:00 +0530

    Add files via upload

---
+++ StringBoolean.java
@@ -0,0 +1,16 @@
+package DSA;
+
+public class StringBoolean 
+{
+	public static void main(String []args)
+	{
+		String str1="Hello";
+		String str2="Rai";
+		boolean x=str1.equals("Rai");
+		System.out.println(x);
+		
+		
+		
+	}
+
+}
