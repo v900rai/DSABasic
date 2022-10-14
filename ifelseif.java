83075b09e630e2f5e36030620837beb0162262ab
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2022-10-14 12:37:07 +0530

    Add files via upload

---
+++ ifelseif.java
@@ -0,0 +1,26 @@
+package DSA;
+
+public class ifelseif 
+{
+	public static void main(String[] args)
+	{
+		int n1=10;
+		int n2=20;
+		if(n1==n2)
+		{
+			System.out.println(n1+"equal to "+n2);
+		}
+		else
+		{
+			if(n1>n2)
+			{
+				System.out.println(n1+"to greater then"+n2);
+			}
+			else
+			{
+				System.out.println(n1+"is smaller then"+n2);
+			}
+		}
+	}
+
+}
