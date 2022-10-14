ce2e4a813b0984fe801e962653acc509857ade8d
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2022-10-14 12:30:19 +0530

    Add files via upload

---
+++ FibonacciNumber.java
@@ -0,0 +1,22 @@
+package DSA;
+import java.util.Scanner;
+public class FibonacciNumber 
+{
+	public static void main(String []args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number");
+		int n=sc.nextInt();
+		int a=0;
+		int b=1;
+		for(int i=0; i<n; i++)
+		{
+			System.out.println(a);
+			int c=a+b;
+			a=b;
+			b=c;
+		}
+		
+	}
+
+}
