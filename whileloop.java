e490202468a1ab989da6eea7e6ec4fbe7e4fcb41
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2022-10-14 17:51:05 +0530

    Add files via upload

---
+++ whileloop.java
@@ -0,0 +1,17 @@
+package DSA;
+
+public class whileloop 
+{
+	public static void main(String []args)
+	{
+		int i=1;
+		while(i<=9)
+		{
+			System.out.println(i);
+			i++;
+		}
+		System.out.println("Hello");
+		System.out.println("Vishal Rai");
+	}
+
+}
