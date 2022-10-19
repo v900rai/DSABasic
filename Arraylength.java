ed1aa06c9f3a9053ab488978e7c3a4051d22ca71
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2022-10-19 21:29:31 +0530

    Add files via upload

---
+++ Arraylength.java
@@ -0,0 +1,23 @@
+package DSA;
+import java.util.Scanner;
+public class Arraylength
+{
+	public static void main(String []args)
+	{
+		//Scanner sc = new Scanner(System.in);
+		//System.out.println("Print the length");
+		int[] arr=new int[5];
+		arr[0]=1;
+		arr[1]=2;
+		arr[2]=3;
+		arr[3]=4;
+		arr[4]=5;
+		for(int i=0; i<arr.length; i++)
+		{
+			System.out.println(arr[i]+" "); 
+		}
+		
+		
+	}
+
+} 
