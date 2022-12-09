5deb0eb58e4d8b1d7608cf13c0743ab027c2d018
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2022-12-09 23:52:32 +0530

    Add files via upload

---
+++ bitwiseoperator.java
@@ -0,0 +1,16 @@
+package NumberProgram;
+
+public class bitwiseoperator 
+{
+	public static void main(System[] args)
+	{
+	int a=5; 
+	int b=7;
+	System.out.println("AND"+(a & b));
+	//System.out.println("OR"+(a|b));
+	//System.out.println("XOR"+(a^b));
+	//System.out.println("compliment"+(a~b));
+	}
+
+}
+                                                                                                                                                                                                                                                                                                                                                                                                                     
