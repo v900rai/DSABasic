5925f998f943d0a8cae08b418b2b036fdb71f2b7
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-05-06 23:22:39 +0530

    Add files via upload

---
+++ TestThrow1.java
@@ -0,0 +1,22 @@
+package javaInterviewProgram;
+
+public class TestThrow1 
+{
+	 //function to check if person is eligible to vote or not   
+    public static void validate(int age) {  
+        if(age<18) {  
+            //throw Arithmetic exception if not eligible to vote  
+            throw new ArithmeticException("Person is not eligible to vote");    
+        }  
+        else {  
+            System.out.println("Person is eligible to vote!!");  
+        }  
+    }  
+    //main method  
+    public static void main(String args[]){  
+        //calling the function  
+        validate(13);  
+        System.out.println("rest of the code...");    
+  }   
+
+}
