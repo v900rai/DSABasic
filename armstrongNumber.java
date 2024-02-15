aa4a2fbb25f4b28ac2fa8481511ce23bbdb740c0
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-02-15 11:14:26 +0530

    Add files via upload

--- armstrongNumber.java
+++ armstrongNumber.java
@@ -1,27 +1,14 @@
-package August;
-import java.util.Scanner;
-public class armstrongNumber 
-{
-	public static void main(String []args)
-	{
-		Scanner sc=new Scanner(System.in);
-		System.out.println("enter the number");
-		int number=sc.nextInt();
-		int ram=0, c;
-		c=number;
-		while(number>0)
-		{
-			int rem=number%10;
-			ram=(rem*rem*rem)+ram;
-			number=number/10;
-		}
-		if(c==ram)
-		{
-			System.out.println("armastrong number");
-		}
-		else {
-		System.out.println("it is not amstrng number");
-		}
-	}
+package JavaInterViewProgram100VVI;
 
+public class armstrongNumber {
+    public static void main(String []args){
+        //153: 13 + 53 + 33 = 1 + 125+ 27 = 153
+        //
+        //125: 13 + 23 + 53 = 1 + 8 + 125 = 134 (Not an Armstrong Number)
+        //
+        //1634: 14 + 64 + 34 + 44 = 1 + 1296 + 81 + 256 = 1643
+        //
+        //Similarly, we can check other number also.
+
+    }
 }
