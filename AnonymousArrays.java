60a45b01c927187f45f56e3b897e112a7e4eb6dc
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-03-17 20:56:18 +0530

    Add files via upload

---
+++ AnonymousArrays.java
@@ -0,0 +1,9 @@
+package ArraysProgram;
+
+import java.util.Arrays;
+
+public class AnonymousArrays {
+    public static void main(String []args){
+        System.out.println(Arrays.toString(new int[] {1, 2, 3, 4, 5}));
+    }
+}
