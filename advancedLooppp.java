d2a3049c01a1879e52e0c6827af3e6fcbb15d8a1
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-03-17 20:57:07 +0530

    Add files via upload

---
+++ advancedLooppp.java
@@ -0,0 +1,10 @@
+package ArraysProgram;
+
+public class advancedLooppp {
+    public static void main(String [] args){
+        int arrr[]= {1,2,3,4,5,6};
+        for( int i : arrr){
+            System.out.println(i);
+        }
+    }
+}
