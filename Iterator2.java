e90023d4cd0b9fbd9b12dbaacf06dba2795c763f
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-04-12 21:10:09 +0530

    Add files via upload

---
+++ Iterator2.java
@@ -0,0 +1,24 @@
+package garaph;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Iterator2 
+{
+	public static void main(String []args) {
+		 // Make a collection
+	    ArrayList<String> cars = new ArrayList<String>();
+	    cars.add("Volvo");
+	    cars.add("BMW");
+	    cars.add("Ford");
+	    cars.add("Mazda");
+
+	    // Get the iterator
+	    Iterator<String> it = cars.iterator();
+
+	    // Print the first item
+	    System.out.println(it.next());
+	  }
+	}
+
+
