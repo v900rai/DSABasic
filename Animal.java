3f3d55afd901a73e295134c9e81349fc1a00c170
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-04-12 21:09:28 +0530

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,21 @@
+package garaph;
+
+public class Animal
+	{  
+		Animal()
+		{
+			System.out.println("animal is created");
+		}  
+	} 
+		class Dog extends Animal{  
+		Dog()
+		{  
+		System.out.println("dog is created");  
+		}  
+		
+		public static void main(String args[]){  
+		Dog d=new Dog();  
+		}  
+		}
+
+
