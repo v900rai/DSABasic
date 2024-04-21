cbadf7d1ed07c7bd3adee3791510c4bfb682d332
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-04-21 21:08:46 +0530

    Add files via upload

---
+++ evenNumber.java
@@ -0,0 +1,14 @@
+package selfpractices;
+
+public class evenNumber {
+    public static void main(String []args){
+        int nums=10;
+        if(nums%2==0){
+            System.out.println("Even number ");
+
+        }
+        else {
+            System.out.println("odd number");
+        }
+    }
+}
