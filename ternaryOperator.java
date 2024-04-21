d95bbfcf512d8e3c21c4545257247a5e8c043a16
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-04-21 21:07:44 +0530

    Add files via upload

---
+++ ternaryOperator.java
@@ -0,0 +1,12 @@
+package selfpractices;
+
+public class ternaryOperator {
+    public static void main(String []args){
+        int a=10;
+        int b=20;
+        int max;
+        max=(a>b) ? a:b;
+        System.out.println(max);
+
+    }
+}
