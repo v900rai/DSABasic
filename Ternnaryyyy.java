9800fea85358bb8093280c10949ca2c94cec6c9a
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-04-22 18:19:18 +0530

    Add files via upload

---
+++ Ternnaryyyy.java
@@ -0,0 +1,9 @@
+package selfpractices;
+
+public class Ternnaryyyy {
+    public static void main(String[] args) {
+        int a=10,b=20,c=30,max;
+        max=(a>b)? (a>b ? a:c):(b>c ?b:c);
+        System.out.println(max);
+    }
+}
