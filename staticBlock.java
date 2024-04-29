5ab5e756a42c6a239cdc3038ece0e4a1988bb184
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-04-29 17:28:31 +0530

    Add files via upload

---
+++ staticBlock.java
@@ -0,0 +1,10 @@
+package selfpractices;
+
+public class staticBlock {
+    static{
+        System.out.println("Static block executed");
+    }
+    public static void main(String []args){
+        System.out.println("main method executed");
+    }
+}
