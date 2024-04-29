f2536c97aeff42e95bec0f51bab9f7718c5ad83e
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-04-29 17:27:09 +0530

    Add files via upload

---
+++ INSTANCSBLOCKCHECK.java
@@ -0,0 +1,13 @@
+package selfpractices;
+
+public class INSTANCSBLOCKCHECK {
+{
+        // instanceblock
+    System.out.println("instance block executed");
+    }
+
+    public static void main(String[] args) {
+      INSTANCSBLOCKCHECK in=new INSTANCSBLOCKCHECK();
+
+    }
+}
