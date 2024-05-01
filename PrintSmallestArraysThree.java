bf60eeb79d3fcef405e9cb2b37b33d434953302c
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-05-01 19:50:04 +0530

    Add files via upload

---
+++ PrintSmallestArraysThree.java
@@ -0,0 +1,40 @@
+package selfpractices;
+
+public class PrintSmallestArraysThree {
+    public static void print3largest(int arr[], int size){
+        int first,second,third;
+        if(size<3){
+            System.out.println("Invalided Input ");
+            return ;
+        }
+        third = first = second = Integer.MAX_VALUE;
+        for(int i=0; i<size; i++){
+            if(arr[i]<first){
+                third=second;
+                second=first;
+                first=arr[i];
+            }
+              /* If arr[i] is in between first and
+            second then update second  */
+            else if(arr[i]<second){
+                third=second;
+                second=arr[i];
+
+            }
+            else if(arr[i]<third)
+                third=arr[i];
+
+            //System.out.println("Three largest elements are " + first + " " + second + " " + third);
+        }
+        System.out.println("Three largest elements are " + first + " " + second + " " + third);
+
+    }
+
+    /* Driver program to test above function*/
+    public static void main(String[] args)
+    {
+        int arr[] = { 12, 13, 1, 10, 34, 1,7,5 };
+        int n = arr.length;
+        print3largest(arr, n);
+    }
+}
