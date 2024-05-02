21d89e0e711aff31a8f44a7c77082350099f3e01
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-05-02 21:15:57 +0530

    Add files via upload

---
+++ trySecondMethod.java
@@ -0,0 +1,14 @@
+package selfpractices;
+
+public class trySecondMethod {
+    public static void main (String[] args) {
+        int a=5;
+        int b=0;
+        try{
+            System.out.println(a/b);
+        }
+        catch(ArithmeticException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
