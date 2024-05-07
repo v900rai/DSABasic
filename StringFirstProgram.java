239f981db44840fe38f1314224ddecb957b62a5f
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-05-07 21:46:59 +0530

    Add files via upload

---
+++ StringFirstProgram.java
@@ -0,0 +1,14 @@
+package selfpractices;
+
+public class StringFirstProgram {
+    public static void main(String []args){
+        String str="vishal rai";
+        // Prints the String.
+        System.out.println("String s = " + str);
+        // Declare String using new operator
+        String s1 = new String("GeeksforGeeks");
+
+        // Prints the String.
+        System.out.println("String s1 = " + s1);
+    }
+}
