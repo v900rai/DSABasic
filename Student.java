c385945389c6553ca44fe817c924f7bdaa6354b1
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-05-22 21:28:20 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,19 @@
+package selfpractices;
+
+ // class definition
+ class Student {
+     String name;
+ }
+
+ class GfG {
+     public static void main(String args[])
+     {
+         // creating an object
+         Student student1 = new Student();
+
+         // assigning member some value
+         student1.name = "Rahul";
+
+         System.out.println("student1.name: " + student1.name);
+     }
+ }
