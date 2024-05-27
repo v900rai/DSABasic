18b5de3536051a7188bff641903f40a7800da6f7
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-05-27 20:45:21 +0530

    Add files via upload

---
+++ Substr2.java
@@ -0,0 +1,13 @@
+package selfpractices;
+
+public class Substr2
+{
+    public static void main(String[] args) {
+        String str= new String(" Welcome to vishal rai your new home ");
+        // using substring() to extract substring
+        // returns geeks
+        System.out.print("The extracted substring  is : ");
+        System.out.println(str.substring(5, 10));
+
+    }
+}
