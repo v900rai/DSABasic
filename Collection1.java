e157aa3208ae3839df5eb175bb2a531c07c2c49e
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-05-27 20:46:10 +0530

    Add files via upload

---
+++ Collection1.java
@@ -0,0 +1,13 @@
+package selfpractices;
+
+import java.util.ArrayList;
+
+public class Collection1 {
+    public static void main(String []args){
+        ArrayList list=new ArrayList();
+        list.add("vishal");
+        list.add("rai");
+        list.add("ajay");
+        System.out.println("Print all the value "+list );
+    }
+}
