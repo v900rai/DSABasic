f0608e6f058574575a7ef7b52f2c489193803bb2
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-05-28 19:17:51 +0530

    Add files via upload

---
+++ RightAngleTriangle.java
@@ -0,0 +1,22 @@
+package selfpractices;
+
+
+    import java.util.Scanner;
+
+    public class RightAngleTriangle {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter the number of rows: ");
+            int rows = scanner.nextInt();
+
+            for (int i = 1; i <= rows; i++) {
+                for (int j = 1; j <= i; j++) {
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+            scanner.close();
+        }
+
+
+}
