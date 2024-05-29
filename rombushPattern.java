6d771da2f9d6fda00be7287693ee41d6040862db
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-05-29 23:14:10 +0530

    Add files via upload

---
+++ rombushPattern.java
@@ -0,0 +1,17 @@
+package selfpractices;
+
+public class rombushPattern {
+    public static void main(String []args){
+        int n=5;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            //star
+            for(int j=1; j<=n; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
