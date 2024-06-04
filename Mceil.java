71bbda442ecafa42cc305996fb657ecc5d5e2874
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-06-04 23:07:52 +0530

    Add files via upload

---
+++ Mceil.java
@@ -0,0 +1,8 @@
+package selfpractices;
+
+public class Mceil {
+    public static void main(String []args){
+        double n=Math.ceil(4.0005d);
+        System.out.println(" print ceil value "+n);
+    }
+}
