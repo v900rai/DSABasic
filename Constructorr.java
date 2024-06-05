a890c052dae3379f2581492999008a6d5c65d451
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-06-05 23:09:00 +0530

    Add files via upload

---
+++ Constructorr.java
@@ -0,0 +1,11 @@
+package selfpractices;
+
+public class Constructorr {
+      public Constructorr(){
+          System.out.println("vishal rai");
+      }
+    public static void main(String[] args)
+    {
+        Constructorr geek = new Constructorr();
+    }
+}
