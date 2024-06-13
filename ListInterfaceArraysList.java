78f6072c9b643d0b913abc6fcf432638b9e0584c
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-06-13 22:43:01 +0530

    Add files via upload

---
+++ ListInterfaceArraysList.java
@@ -0,0 +1,19 @@
+package selfpractices.InterviewOnly;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListInterfaceArraysList {
+    public static void main(String[] args) {
+
+        // create arraylist using List
+        List<String> languages = new ArrayList<>();
+        languages.add("Java");
+        languages.add("Python");
+        languages.add("C");
+
+        System.out.println("ArrayList: " + languages);
+    }
+}
+
+
