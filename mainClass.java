35b0f4a32da07966f12d0c8e35aebe6e92c9715c
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-07-10 19:57:35 +0530

    Add files via upload

---
+++ mainClass.java
@@ -0,0 +1,9 @@
+package JavaOops.toString;
+
+public class mainClass {
+    public static void main(String []args){
+        Strudent st=new Strudent(111, "vishal","sant kabir");
+        System.out.println(st);
+
+    }
+}
