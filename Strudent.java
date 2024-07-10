c78f680c99b17d1ec5b523faabc15a0bfa010709
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-07-10 19:57:20 +0530

    Add files via upload

---
+++ Strudent.java
@@ -0,0 +1,19 @@
+package JavaOops.toString;
+
+public class Strudent {
+    int rollnumber;
+    String name;
+    String add;
+    public  Strudent(int rollnumber,String name,String add){
+        this.rollnumber=rollnumber;
+        this.name=name;
+        this.add=add;
+        
+    }
+
+  //  public Strudent(int i, String vishal, String santKabir) {
+  //  }
+//    public  String toString(){
+//        return  rollnumber+" "+name+" "+add;
+//    }
+}
