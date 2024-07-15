d470e39105883b5e6ebc072d88d8b888040a85a0
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-07-15 19:25:05 +0530

    Add files via upload

---
+++ basicLoopingStatements.java
@@ -0,0 +1,11 @@
+package loopingStatementsInJava;
+
+public class basicLoopingStatements {
+    public static void main(String[] args) {
+        int n=9;
+        for(int i=1; i<n; i++){
+            System.out.println(i);
+    }
+
+    }
+}
