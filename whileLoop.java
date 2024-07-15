7c2ced9b1fdcaccba6581ad4635431330ca2ec16
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-07-15 19:25:24 +0530

    Add files via upload

---
+++ whileLoop.java
@@ -0,0 +1,14 @@
+package loopingStatementsInJava;
+
+public class whileLoop {
+    public static void main(String[] args) {
+        int i=10;
+        while (i>=1){
+            System.out.print(i+" ,");
+            i--;
+        }
+
+    }
+
+    }
+
