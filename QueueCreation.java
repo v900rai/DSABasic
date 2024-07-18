1744b49f000b6ea8cce3928cb5af6c41ef8a4ace
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-07-18 20:42:20 +0530

    Add files via upload

---
+++ QueueCreation.java
@@ -0,0 +1,18 @@
+package AdvancedDataStructureInJava;
+
+
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueCreation {
+    public static void main(String args[]) {
+
+        // Create a Queue of Integers using LinkedList
+        Queue<Integer> queue = new LinkedList<>();
+
+        // Displaying the Queue
+        System.out.println("Queue elements: " + queue);
+    }
+}
