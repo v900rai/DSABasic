85787a0084dff05f0a378227835b1a59e073b9b4
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-07-21 22:33:31 +0530

    Add files via upload

--- AsciiValueOfAllChracters.java
+++ AsciiValueOfAllChracters.java
@@ -1,12 +1,21 @@
-package allProgram;
+package edu.jspider.heca48.twoD.Array;
 
 public class AsciiValueOfAllChracters 
 {
-	public static void main(String [] args)
+	public static void main(String []args)
 	{
-		for(int i=0; i<=225; i++)
-		{
-			System.out.println("AsciiValueOfAllChracters"+(char)i+" = "+i);
-		}
+		for(int i = 0; i <= 255; i++)  
+		{  
+		System.out.println(" The ASCII value of " + (char)i + "  =  " + i);  
+		break;
+		} 
+		for(int i = 65; i <= 90; i++)  
+		{  
+		System.out.println(" The ASCII value of " + (char)i + "  =  " + i);  
+		}  
 	}
+	/*Similarly, we can print the ASCII value of a to z by changing the loop in the above code.
+
+	for(int i = 97; i <= 122; i++) */ 
+
 }
