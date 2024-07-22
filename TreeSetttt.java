8b5b1ebc91c068d26c980f0945432672e6fecd39
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-07-22 19:01:13 +0530

    Add files via upload

---
+++ TreeSetttt.java
@@ -0,0 +1,18 @@
+package Hashmap;
+import java.util.TreeSet;
+public class TreeSetttt {
+
+
+        public static void main(String[] args) {
+            TreeSet<String> tree_set = new TreeSet<String>();
+            tree_set.add("Red");
+            tree_set.add("Green");
+            tree_set.add("Orange");
+            tree_set.add("White");
+            tree_set.add("Black");
+            System.out.println("Tree set: ");
+            System.out.println(tree_set);
+        }
+    }
+
+
