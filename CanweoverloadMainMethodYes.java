244c499bfc91e1a1af5a1e37817ae7663dfd9cb4
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-08-11 13:34:11 +0530

    Add files via upload

---
+++ CanweoverloadMainMethodYes.java
@@ -0,0 +1,10 @@
+package OOPSJAVA.POLYMORSIM;
+
+public class CanweoverloadMainMethodYes {
+
+        public static void main(String[] args){System.out.println("main with String[]");}
+        public static void main(String args){System.out.println("main with String");}
+        public static void main() {
+        }
+
+        }
