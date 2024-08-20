d320e68f2c886f25e1e5b892514454199295c2e0
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-08-20 19:21:54 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,12 @@
+package AdvancedDataStructureInJava;
+
+public class sum {
+    public static void main(String args[]){
+        int arr[]={1,2,3,4,5};
+        int sum=0;
+        for(int i=0; i<arr.length; i++){
+            sum=sum+arr[i];
+        }
+        System.out.println(sum);
+    }
+}
